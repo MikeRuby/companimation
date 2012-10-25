02288020c5422bfc5d8b17681dbeb4990cecdd05
Mike Fowler <mike@mikefowler.me>
2012-10-25 08:15:08 -0400

    adding some purty animations

--- index.md
+++ index.md
@@ -5,11 +5,9 @@ title: Companimation
 
 ## Overview
 
-_Another animation extension for Compass? Seriously? But Mike, Why?_
+_Another animation extension for Compass? Seriously? But Mike, Why?_ Because this one does it better.
 
-The truth is, I use Compass a lot. Recently I've started using animations a lot more on certain projects, so naturally I started looking for a better way to handle them since currently Compass does not provide any mixins for animations. So... I looked around at the extensions already available and decided I'd rather do it my way. So I did. HUZZAH!
-
-Companimation provides two unique things: a basic set of mixins to help you create your own animations, and a collection of presets (both original and sourced from [Dan Eden's Animate.css](http://daneden.me/animate/) to get you up and running fast.
+Companimation provides two unique things: a basic set of mixins to help you create your own animations super quick, and a collection of presets (both original and sourced from [Dan Eden's Animate.css](http://daneden.me/animate/)) to get you up and running fast.
 
 ## Installation and Usage
 
@@ -38,7 +36,8 @@ Using presets in Companimation is a breeze. Presets take advantage of Sass 3.2 p
 
 {% highlight scss %}
 @import 'companimation';
-@import 'companimation/preset/fade';
+@import 'companimation/preset/fade/fadeIn';
+@import 'companimation/preset/fade/fadeInUp';
 
 body {
   @extend %fadeIn;
--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
 (function() {
 
-    var needle;
+    var needle,
+        scrollSegment = 220,
+        minBodyWidth = 630;
 
     function scrollHandler() {
-        var scrollSegment = 200,
-            scrollTop,
+        var scrollTop,
             percentage,
             rotation;
 
         scrollTop = document.body.scrollTop;
 
-        percentage = ((scrollTop % scrollSegment) / scrollSegment) * 0.5;
+        percentage = ((scrollTop % 800) / 800);
         rotation = 360 * percentage;
 
-        needle.style.webkitTransform = "rotate(" + parseInt(rotation) + "deg)";
-        needle.style.mozTransform = "rotate(" + parseInt(rotation) + "deg)";
-        needle.style.oTransform = "rotate(" + parseInt(rotation) + "deg)";
-        needle.style.transform = "rotate(" + parseInt(rotation) + "deg)";
+        needle.style.webkitTransform = "rotate(" + parseInt(rotation, 0) + "deg)";
+        needle.style.mozTransform = "rotate(" + parseInt(rotation, 0) + "deg)";
+        needle.style.oTransform = "rotate(" + parseInt(rotation, 0) + "deg)";
+        needle.style.transform = "rotate(" + parseInt(rotation, 0) + "deg)";
+
+        fixLogoToWindow();
 
     }
 
     function loaded() {
         needle = document.getElementsByClassName('banner-logo-inner')[0];
+
         window.addEventListener('scroll', scrollHandler);
+        window.addEventListener('resize', fixLogoToWindow);
+    }
+
+    function fixLogoToWindow() {
+        var horizontalCondition,
+            verticalCondition,
+            bodyClass = "fixed-logo";
+
+        horizontalCondition = window.outerWidth >= minBodyWidth + scrollSegment;
+        verticalCondition = document.body.scrollTop >= scrollSegment;
+
+        if (horizontalCondition && verticalCondition) {
+            document.body.className = bodyClass;
+        } else {
+            document.body.className = "";
+        }
     }
 
     window.addEventListener('load', loaded, false);
